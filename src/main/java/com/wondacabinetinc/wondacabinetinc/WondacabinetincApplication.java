e69387bad2c8a385767f3d848f0df4858af00350
Vlad Popa <vpopa18@gmail.com>
2021-11-29 15:46:43 -0500

    Set Up Project

---
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/WondacabinetincApplication.java
@@ -0,0 +1,13 @@
+package com.wondacabinetinc.wondacabinetinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WondacabinetincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WondacabinetincApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wondacabinetinc/wondacabinetinc/WondacabinetincApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wondacabinetinc.wondacabinetinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WondacabinetincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
