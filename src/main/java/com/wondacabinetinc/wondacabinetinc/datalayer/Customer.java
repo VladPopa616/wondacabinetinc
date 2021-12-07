f6699279cbbc0b8ed757e22a4f2eb759ce8e2b7a
Vlad Popa <vpopa18@gmail.com>
2021-12-07 16:13:36 -0500

    Configured models. Some used for later.

---
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/datalayer/Customer.java
@@ -0,0 +1,92 @@
+package com.wondacabinetinc.wondacabinetinc.datalayer;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long accountID;
+
+    @Column(name="first_name")
+    @NonNull
+    private String firstName;
+
+    @Column(name="last_name")
+    @NonNull
+    private String lastName;
+
+    @Column(name="email")
+    @NonNull
+    private String email;
+
+    @Column(name="password")
+    @NonNull
+    private String password;
+
+    public Customer() {
+    }
+
+    public Customer(long accountID, @NonNull String firstName, @NonNull String lastName, @NonNull String email, @NonNull String password) {
+        this.accountID = accountID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Customer(@NonNull String firstName, @NonNull String lastName, @NonNull String email, @NonNull String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(long accountID) {
+        this.accountID = accountID;
+    }
+
+    @NonNull
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NonNull String firstName) {
+        this.firstName = firstName;
+    }
+
+    @NonNull
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NonNull String lastName) {
+        this.lastName = lastName;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    @NonNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NonNull String password) {
+        this.password = password;
+    }
+}
