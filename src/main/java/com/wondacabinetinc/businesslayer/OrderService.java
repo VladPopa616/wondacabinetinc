9a196d54041b411c2e49982dba8a2c4f28194492
Vlad Popa <vpopa18@gmail.com>
2021-12-07 19:58:51 -0500

    Added Business Layer

---
+++ src/main/java/com/wondacabinetinc/businesslayer/OrderService.java
@@ -0,0 +1,11 @@
+package com.wondacabinetinc.businesslayer;
+
+import com.wondacabinetinc.datalayer.Order;
+
+import java.util.List;
+
+public interface OrderService {
+
+    public List<Order> getAllOrders();
+
+}
