fba16b4f9ff327c08d1c55e24ea1635313f451dc
Vlad Popa <vpopa18@gmail.com>
2021-12-13 14:24:23 -0500

    Created SQL query in OrderRepository to inner join order details with Order

--- src/main/java/com/wondacabinetinc/wondacabinetinc/datalayer/OrderRepository.java
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/datalayer/OrderRepository.java
@@ -1,9 +1,17 @@
 package com.wondacabinetinc.wondacabinetinc.datalayer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
+    @Query("select o.orderId, o.trackingNo, o.orderStatus " +
+            "FROM Order o " +
+            "INNER JOIN o.orderDetails od where od.orderId = :order_id")
+    Optional<Order> getDetails(@Param("order_id") Integer order_id);
 }
