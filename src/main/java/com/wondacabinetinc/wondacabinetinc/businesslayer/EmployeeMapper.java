534e1031cec0b0a562b407c7c6d5078abdc04297
VladPopa616 <vpopa18@gmail.com>
2022-01-20 13:21:31 -0500

    Added MapStruct dependencies and created Employee Mapper

---
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/businesslayer/EmployeeMapper.java
@@ -0,0 +1,23 @@
+package com.wondacabinetinc.wondacabinetinc.businesslayer;
+
+import com.wondacabinetinc.wondacabinetinc.datalayer.Employee;
+import com.wondacabinetinc.wondacabinetinc.datalayer.EmployeeDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface EmployeeMapper {
+    @Mappings(
+            {
+                    @Mapping(target = "id", ignore = true),
+            }
+    )
+    Employee employeeDTOtoEmployee(EmployeeDTO employeeDTO);
+    EmployeeDTO employeeToEmployeeDTO(Employee employee);
+
+    List<Employee> employeeDTOListToEmployeeList(List<EmployeeDTO> employeeDTOList);
+    List<EmployeeDTO> employeeListToEmployeeDTOList(List<Employee> employeeList);
+}
