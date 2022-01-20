938b3aa383e0cecf69650f8e85ee6f3a39bc1623
VladPopa616 <vpopa18@gmail.com>
2022-01-20 14:09:46 -0500

    Added JWT Response and Message Response

---
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/jwt/JWTResponse.java
@@ -0,0 +1,68 @@
+package com.wondacabinetinc.wondacabinetinc.jwt;
+
+import java.util.List;
+
+public class JWTResponse {
+    private String token;
+    private String type = "Bearer";
+    private Integer id;
+    private String username;
+    private String email;
+    private List<String> roles;
+
+    public JWTResponse(String token, Integer id, String username, String email, List<String> roles) {
+        this.token = token;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getAccessToken() {
+        return token;
+    }
+
+    public void setAccessToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getEmployeeId() {
+        return id;
+    }
+
+    public void setEmployeeId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
