9e10753725b7579a966ad25742cb09229f98a099
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2022-01-25 21:46:29 -0500

    Partially working PDF email

--- src/main/java/com/wondacabinetinc/wondacabinetinc/Mail/MailSenderService.java
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/Mail/MailSenderService.java
@@ -1,10 +1,12 @@
 package com.wondacabinetinc.wondacabinetinc.Mail;
 
+import javax.mail.MessagingException;
+
 public interface MailSenderService {
 
 //    public void sendSimpleEmail(Mail mail);
 
     public void sendEmail(String toEmail, String body, String subject );
 
-    public void sendEmailWithAttachment(String toEmail, String body, String subject, String attachment );
+    public void sendEmailWithAttachment(String toEmail, String body, String subject, String attachment ) throws MessagingException;
 }
