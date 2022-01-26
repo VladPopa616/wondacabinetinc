8a28dd634aad0ad6cce48d0a22771662754ed774
LawrenceLiang02 <lawrencekaiwei@gmail.com>
2022-01-25 21:45:57 -0500

    Partially working PDF email

--- src/main/java/com/wondacabinetinc/wondacabinetinc/Mail/MailSenderService.java
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/Mail/MailSenderService.java
@@ -4,5 +4,7 @@ public interface MailSenderService {
 
 //    public void sendSimpleEmail(Mail mail);
 
-    public void sendSimpleEmail(String toEmail, String body, String subject );
+    public void sendEmail(String toEmail, String body, String subject );
+
+    public void sendEmailWithAttachment(String toEmail, String body, String subject, String attachment );
 }
--- src/main/java/com/wondacabinetinc/wondacabinetinc/Mail/MailSenderServiceImp.java
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/Mail/MailSenderServiceImp.java
@@ -1,9 +1,15 @@
 package com.wondacabinetinc.wondacabinetinc.Mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 public class MailSenderServiceImp implements  MailSenderService{
@@ -12,7 +18,7 @@ public class MailSenderServiceImp implements  MailSenderService{
     private JavaMailSender mailsender;
 
     @Override
-    public void sendSimpleEmail(String toEmail, String body, String subject ){
+    public void sendEmail(String toEmail, String body, String subject ){
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setFrom("noreply.wondacabinetinc@gmail.com");
@@ -21,5 +27,26 @@ public class MailSenderServiceImp implements  MailSenderService{
         message.setSubject(subject);
 
         mailsender.send(message);
+    }
+
+    @Override
+    public void sendEmailWithAttachment(String toEmail, String body, String subject, String attachment) throws MessagingException {
+        MimeMessage mimeMessage = mailsender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper
+                = new MimeMessageHelper(mimeMessage, true);
+
+        mimeMessageHelper.setFrom("spring.email.from@gmail.com");
+        mimeMessageHelper.setTo(toEmail);
+        mimeMessageHelper.setText(body);
+        mimeMessageHelper.setSubject(subject);
+
+        FileSystemResource fileSystem
+                = new FileSystemResource(new File(attachment));
+
+        mimeMessageHelper.addAttachment(fileSystem.getFilename(),
+                fileSystem);
+
+        mailsender.send(mimeMessage);
     }
 }
