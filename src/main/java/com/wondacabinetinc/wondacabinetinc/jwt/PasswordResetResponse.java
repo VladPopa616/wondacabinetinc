8ea685bf80c85db1823311724f0428f5d1d4596e
VladPopa616 <vpopa18@gmail.com>
2022-02-10 17:01:47 -0500

    Added password reset request and response

---
+++ src/main/java/com/wondacabinetinc/wondacabinetinc/jwt/PasswordResetResponse.java
@@ -0,0 +1,14 @@
+package com.wondacabinetinc.wondacabinetinc.jwt;
+
+public class PasswordResetResponse {
+
+    private String responseMessage;
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
